a2a9062dbd1360f41b8bc64ac26de87db749c2a2
Mario Rossi <veontomo@gmail.com>
2015-12-29 16:12:26 +0100

    feat: elaborate commit-click handler

--- src/emulatedWeb/statistics/commit/index.php
+++ src/emulatedWeb/statistics/commit/index.php
@@ -1,3 +1,3 @@
 <?php
       header('Content-type: application/json');
-      echo json_encode(["dumb" => true, "now" => date('H:i:s', time())]);
+      echo json_encode(["dumb" => true, "now" => date('H:i:s', time()), "size" => 21]);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,20 +1,46 @@
 (function () {
-    var server = "emulatedWeb/";
-    var commitUrl = server + "statistics/commit/index.php";
-    $("#commitBtn").click(function () {
+    var commitUrl = "emulatedWeb/statistics/commit/index.php";
+    console.log(commitUrl);
+    $("#commitBtn").click(function (event) {
         var name = $("#name").val();
-        var pswd = $("#pwd").val();
-        console.log(name, pswd);
-        var data = {"name": name, "password": pswd};
-        jQuery.post(commitUrl, data, function (recievedData, status, jqXHR) {
-            console.log(recievedData, status, jqXHR);
-        }).done(function (msg) {
-            console.log("second success", msg);
-        }).fail(function (msg) {
+        var password = $("#pwd").val();
+        $("#name").val("");
+        $("#password").val("");
+        var data = {"name": name, "password": password};
+        jQuery.post(commitUrl, data, function (data, status, jqXHR) {
+            console.log(data, status, jqXHR);
+        }).done(onResponceReceived).fail(function (msg) {
             console.log("error: ", msg);
         }).always(function () {
             console.log("finished");
         });
     });
+
+    var onResponceReceived = function (msg){
+        var node = $('#message');
+        var text;
+
+        if (msg && msg.size >= 0) {
+            text = "Sono stati marcati " + msg.size + " record per il prossimo salvataggio";
+            switchNodeText(node, text, "text-success", "text-danger");
+        } else {
+            text = msg.errorCode >= 0 ? "Codice errore: " + msg.errorCode : "Errore non documentato";
+            switchNodeText(node, text, "text-danger", "text-success");
+        }
+    };
+
+    /**
+     * Switch a class of a given node
+     * @param nodeId
+     * @param text
+     * @param addClass
+     * @param removeClass
+     */
+    var switchNodeText = function(node, text, addClass, removeClass){
+        node.text(text);
+        node.addClass(addClass);
+        node.removeClass(removeClass);
+    }
+
 
 })();
