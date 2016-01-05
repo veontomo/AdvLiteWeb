a1282743fa6af0ce321e2e260ba169eca6acccd6
Mario Rossi <veontomo@gmail.com>
2016-01-05 16:51:48 +0100

    feat: configure ajax requests to the server

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,26 @@
 (function () {
-    var commitUrl = "emulatedWeb/statistics/commit/index.php";
-    console.log(commitUrl);
+    var commitUrl = "news/statistics/commit";
+
     $("#commitBtn").click(function (event) {
         var name = $("#name").val();
         var password = $("#pwd").val();
 
         $("#name").val("");
-        $("#password").val("");
+        $("#pwd").val("");
         var preloader = document.createElement('img');
         preloader.src = 'img/preloader.gif';
         var data = {"auth": {"login": name, "password": password}};
+        console.log("to be sent by ajax: ", data);
         addNode($('#message'), preloader);
-        jQuery.post(commitUrl, data).done(onResponceReceived).fail(onCommitFailure).always(function () {
+        $.ajax({
+            type: "POST",
+            url: commitUrl,
+            data: data,
+            contentType: 'application/json',
+            dataType: 'json',
+            async: true,
+            processData: false
+        }).done(onResponceReceived).fail(onCommitFailure).always(function () {
             removeNode(preloader);
         });
     });
@@ -23,7 +32,7 @@
     var onResponceReceived = function (msg) {
         var node = $('#message');
         var text;
-
+        console.log("Received: ", msg);
         if (msg && msg.size >= 0) {
             text = "Sono stati marcati " + msg.size + " record per il prossimo salvataggio";
             switchNodeText(node, text, "text-success", "text-danger");
@@ -38,6 +47,7 @@
      * @param msg json object
      */
     var onCommitFailure = function (msg) {
+        console.log("failed: ", msg);
         var node = $('#message');
         var text = "Errore nella comunicazione con il server: codice " + msg.status + ", messaggio \"" + msg.statusText + "\".";
         switchNodeText(node, text, "text-danger", "text-success");
